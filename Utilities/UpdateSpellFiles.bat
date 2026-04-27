765f5340be0565afde997c19ffe7159a01c9f5dd
Adam Liverman <livermana@gmail.com>
2026-04-26 20:37:47 -0400

    initial commit

---
+++ Utilities/UpdateSpellFiles.bat
@@ -0,0 +1,8 @@
+powershell -Command "Invoke-WebRequest http://15.235.113.14:3000/api/v1/eqemuserver/export-client-file/spells -OutFile '.\spells_us.txt'"
+powershell -Command "Invoke-WebRequest http://15.235.113.14:3000/api/v1/eqemuserver/export-client-file/dbstring -OutFile '.\dbstr_us.txt'"
+
+powershell Copy-Item '.\spells_us.txt' -Destination 'C:\Project Lazarus\RoF2\' -force
+powershell Copy-Item '.\spells_us.txt' -Destination 'C:\Project Lazarus\RoF3\' -force
+
+powershell Copy-Item '.\dbstr_us.txt' -Destination 'C:\Project Lazarus\RoF2\' -force
+powershell Copy-Item '.\dbstr_us.txt' -Destination 'C:\Project Lazarus\RoF3\' -force
